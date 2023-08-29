b30ce75c5504b5e18765ac8548177c58bf138986
ulfunhy <myfamily11141@gmail.com>
2023-08-29 20:12:07 +0900

    OptionMenu 실습 7-1 완성

--- app/src/main/java/kr/hs/emirim/s2233/optionmenutest/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/s2233/optionmenutest/MainActivity.kt
@@ -1,8 +1,10 @@
 package kr.hs.emirim.s2233.optionmenutest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.LinearLayout
 
@@ -24,5 +26,46 @@ class MainActivity : AppCompatActivity() {
         mInflater.inflate(R.menu.menu1, menu)
 
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.itemRed -> {
+                linear.setBackgroundColor(Color.RED)
+                return true;// 정상적인 실행이 되었으므로 true를 리턴
+            }
+
+            R.id.itemBlue -> {
+                linear.setBackgroundColor(Color.BLUE)
+                return true;// 정상적인 실행이 되었으므로 true를 리턴
+            }
+
+            R.id.itemMagenta -> {
+                linear.setBackgroundColor(Color.MAGENTA)
+                return true;// 정상적인 실행이 되었으므로 true를 리턴
+            }
+
+            R.id.subRotate -> {
+                btn.rotation += 45f
+                return true
+            }
+
+            R.id.minusSize -> {
+                btn.scaleX -= 2f
+                return true
+            }
+
+            R.id.SubSize -> {
+                btn.scaleX += 2f// 2배
+                return true
+            }
+
+            R.id.subInit -> {
+                btn.scaleX = 1f
+                btn.rotation = 0f
+                return true
+            }
+        }
+        return false;// 항목이 아닌 부분을 클릭했을 때 실행이 되지 않기 위함
     }
 }
